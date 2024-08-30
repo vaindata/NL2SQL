cade385028a783333f121e152da594446ce42b81
Debojyoti Biswas <53976520+vaindata@users.noreply.github.com>
2024-08-30 20:40:39 +0530

    Logging and scalability added

--- main_file.py
+++ main_file.py
@@ -9,188 +9,47 @@ import sqlite3
 from tqdm import tqdm
 import sqlglot
 from dotenv import load_dotenv
+import sys 
+import logging
+
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 openai.api_key = openai_api_key
 
-def get_context_msg():
-    context = [ {'role':'system', 'content':"""
-    CREATE TABLE "cards"
-(
-    id                      INTEGER           not null
-        primary key autoincrement,
-    artist                  TEXT,
-    asciiName               TEXT,
-    availability            TEXT,
-    borderColor             TEXT,
-    cardKingdomFoilId       TEXT,
-    cardKingdomId           TEXT,
-    colorIdentity           TEXT,
-    colorIndicator          TEXT,
-    colors                  TEXT,
-    convertedManaCost       REAL,
-    duelDeck                TEXT,
-    edhrecRank              INTEGER,
-    faceConvertedManaCost   REAL,
-    faceName                TEXT,
-    flavorName              TEXT,
-    flavorText              TEXT,
-    frameEffects            TEXT,
-    frameVersion            TEXT,
-    hand                    TEXT,
-    hasAlternativeDeckLimit INTEGER default 0 not null,
-    hasContentWarning       INTEGER default 0 not null,
-    hasFoil                 INTEGER default 0 not null,
-    hasNonFoil              INTEGER default 0 not null,
-    isAlternative           INTEGER default 0 not null,
-    isFullArt               INTEGER default 0 not null,
-    isOnlineOnly            INTEGER default 0 not null,
-    isOversized             INTEGER default 0 not null,
-    isPromo                 INTEGER default 0 not null,
-    isReprint               INTEGER default 0 not null,
-    isReserved              INTEGER default 0 not null,
-    isStarter               INTEGER default 0 not null,
-    isStorySpotlight        INTEGER default 0 not null,
-    isTextless              INTEGER default 0 not null,
-    isTimeshifted           INTEGER default 0 not null,
-    keywords                TEXT,
-    layout                  TEXT,
-    leadershipSkills        TEXT,
-    life                    TEXT,
-    loyalty                 TEXT,
-    manaCost                TEXT,
-    mcmId                   TEXT,
-    mcmMetaId               TEXT,
-    mtgArenaId              TEXT,
-    mtgjsonV4Id             TEXT,
-    mtgoFoilId              TEXT,
-    mtgoId                  TEXT,
-    multiverseId            TEXT,
-    name                    TEXT,
-    number                  TEXT,
-    originalReleaseDate     TEXT,
-    originalText            TEXT,
-    originalType            TEXT,
-    otherFaceIds            TEXT,
-    power                   TEXT,
-    printings               TEXT,
-    promoTypes              TEXT,
-    purchaseUrls            TEXT,
-    rarity                  TEXT,
-    scryfallId              TEXT,
-    scryfallIllustrationId  TEXT,
-    scryfallOracleId        TEXT,
-    setCode                 TEXT,
-    side                    TEXT,
-    subtypes                TEXT,
-    supertypes              TEXT,
-    tcgplayerProductId      TEXT,
-    text                    TEXT,
-    toughness               TEXT,
-    type                    TEXT,
-    types                   TEXT,
-    uuid                    TEXT              not null
-        unique,
-    variations              TEXT,
-    watermark               TEXT
-);
+log_file_path = 'error.log'
+if os.path.exists(log_file_path):
+    os.remove(log_file_path)
 
-    CREATE TABLE "foreign_data"
-(
-    id           INTEGER not null
-        primary key autoincrement,
-    flavorText   TEXT,
-    language     TEXT,
-    multiverseid INTEGER,
-    name         TEXT,
-    text         TEXT,
-    type         TEXT,
-    uuid         TEXT
-        references cards (uuid)
-);
-                 
-CREATE TABLE "legalities"
-(
-    id     INTEGER not null
-        primary key autoincrement,
-    format TEXT,
-    status TEXT,
-    uuid   TEXT
-        references cards (uuid)
-            on update cascade on delete cascade
-);
+logging.basicConfig(
+    filename=log_file_path,            
+    level=logging.ERROR,          
+    format='%(asctime)s - %(levelname)s - %(message)s' 
+)
 
-CREATE TABLE "rulings"
-(
-    id   INTEGER not null
-        primary key autoincrement,
-    date DATE,
-    text TEXT,
-    uuid TEXT
-        references cards (uuid)
-            on update cascade on delete cascade
-);
+def get_context_msg(schema):
 
-CREATE TABLE "set_translations"
-(
-    id          INTEGER not null
-        primary key autoincrement,
-    language    TEXT,
-    setCode     TEXT
-        references sets (code)
-            on update cascade on delete cascade,
-    translation TEXT
-);
-
-CREATE TABLE "sets"
-(
-    id               INTEGER           not null
-        primary key autoincrement,
-    baseSetSize      INTEGER,
-    block            TEXT,
-    booster          TEXT,
-    code             TEXT              not null
-        unique,
-    isFoilOnly       INTEGER default 0 not null,
-    isForeignOnly    INTEGER default 0 not null,
-    isNonFoilOnly    INTEGER default 0 not null,
-    isOnlineOnly     INTEGER default 0 not null,
-    isPartialPreview INTEGER default 0 not null,
-    keyruneCode      TEXT,
-    mcmId            INTEGER,
-    mcmIdExtras      INTEGER,
-    mcmName          TEXT,
-    mtgoCode         TEXT,
-    name             TEXT,
-    parentCode       TEXT,
-    releaseDate      DATE,
-    tcgplayerGroupId INTEGER,
-    totalSetSize     INTEGER,
-    type             TEXT
-);            
-
-"""} ]
-
-    
+    context = [ {'role':'system', 'content':schema}]
     return context
 
 def text_to_sql(question):
+    tables = get_table_names()
+
     prompt = f"""Given the following tables:
-    `cards`,`foreign_data`,`legalities` ,`rulings`,`set_translations`,`sets`
+    {tables}
 
 Convert the following question to SQL, and please don't give any unnecessary character in output:
 Question: {question}
 
 SQL:"""
-    
-    context = get_context_msg()
+    schema = get_schemas()
+    context = get_context_msg(schema)
     context.append({'role': 'user', 'content': prompt})
 
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",  # You can also use "gpt-4" if you have access
         messages=context,
-        temperature=0.7
+        temperature=0.9
     )
     return response.choices[0].message.content.strip('```').strip('sql')
 
@@ -201,14 +60,42 @@ def compare_results(df1, df2):
         return True
     return len(df1) == len(df2)
 
-with open('dev.json') as f:
+# Dev file for BIRD
+path = 'dev.json'
+with open(path) as f:
     data = json.load(f)
 
-data = [d for d in data if d['db_id'] == 'card_games']
+def get_table_names():
+    cursor = conn.cursor()
+    try:
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cursor.fetchall()
+        tables = [t[0] for t in tables]
+    except Exception as e:
+        logging.error("An error occurred connecting %s db %s\n", db_name, e)
+    return ','.join(tables)
+
+def get_schemas():
+    cursor = conn.cursor()
+    try:
+
+        cursor.execute("SELECT sql FROM sqlite_master WHERE type='table';")
+        table_schema = cursor.fetchall()
+        table_schema = [t[0] for t in table_schema]
+    except Exception as e:
+        logging.error("An error occurred connecting %s db %s\n", db_name, e)
+
+    return '\n'.join(table_schema)
+
+
+
+db_name = sys.argv[1]
+
+conn = sqlite3.connect(db_name + '.sqlite')
+
+data = [d for d in data if d['db_id'] == db_name]
 random.shuffle(data)
 data = data[:30]
-
-conn = sqlite3.connect('card_games.sqlite')
 
 def validate_sql_expression(sql_expression):
     try:
@@ -216,7 +103,7 @@ def validate_sql_expression(sql_expression):
         print("The SQL expression is valid.")
         return parsed_expression
     except Exception as e:
-        print(f"Invalid SQL expression.")
+        logging.error("Invalid SQL expression %s\n", e)
 
 
 total, pred = 0, 0
@@ -233,7 +120,7 @@ for i, d in tqdm(enumerate(data)):
         if flag:
             pred += 1
     except Exception as e:
-        pass
+        logging.error('Error occurred fetching results %s\n', e)
     total += 1
 
 accuracy = pred / total
